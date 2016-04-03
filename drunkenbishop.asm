0d090d6015f56cd3c5769f2e5839c54d93728263
mikeg19 <mkg.grabowski@gmail.com>
2016-04-03 19:27:36 +0200

    Done: -everything ready and working

--- drunkenbishop.asm
+++ drunkenbishop.asm
@@ -26,12 +26,15 @@ data segment
 	newline 					db 10,13, '$' 
  	debug  						db "*$"
  	tmp							db 0, '$'
+ 	horray						db "Doing some mods$"
  	;/debugging args
  	
  	; drunken bishop section
  	chartable					db 153 	dup(0)
  	bytesequence0				db 16  	dup(0)
  	bytesequence1				db 17  	dup(0)
+ 	topframe					db "+--( RSA 1024)----+$"
+ 	bottomframe					db "+-----------------+$"
  	;/drunken bishop section
 
  	; errors
@@ -57,10 +60,9 @@ main:
   	mov sp, offset stack_top
 
   	call parse
-  	mov dx, offset newline
-  	call print
   	call drunkenbishop
-    ;end the program end exit to command prompt
+   
+   	 ;end the program end exit to command prompt
     call endsection
 
 ;###################################              Parser              ################################### 
@@ -73,9 +75,6 @@ parse: 								; parse arguments, calls proper procedures
 	call setarglengths
 	call buffertoargs
 	call validateargs
-	;print arguments
-	mov dx, offset buffer			
-	call print
 ret
 
 loadtobuffer:						; load from PSP buffer to var buffer in loop
@@ -375,6 +374,10 @@ ret
 drunkenbishop:						; translate to binary, make moves
 
 	call arg2tobytes
+	call modification
+	call makeallmoves
+	call makefingerprint
+	call printfingerprint
 ret
 
 arg2tobytes: 						; translate hexadecimal bytes to binary
@@ -445,6 +448,386 @@ asciitobyte:						; translate two ascii chars to a byte, return byte in dl, conf
 	pop cx
 ret
 
+modification:						; check if make modified asciiart, and prepare bytesequence0 for it #confirmed working
+
+	push ax
+	push cx
+	xor ax, ax
+	mov ax, seg data
+	mov ds, ax
+	xor ax, ax
+
+	mov al, byte ptr ds:[argument1]
+	cmp al, 49						; check if argument1=='1'
+	JNE nomod						; if argument1!='1', proceed to making moves
+
+	
+	writesequence0to1:				; write bytes from bytesequence0 to bytesequence1 for easier xor later
+	xor ax, ax						; ax=0
+	xor cx, cx 						; cx=0
+	mov al, byte ptr ds:[bytesequence0 +15]; copy last bytesequence0 char to first index in bytesequence1
+	mov byte ptr ds:[bytesequence1], al
+	xor al, al
+	mov si, offset bytesequence0 	; now si (source index) is pointing to first char of bytesequence0
+	mov di, offset bytesequence1 	
+	inc di 							; now di (destination index) is pointing to second char of bytesequence1 (first is already copied)
+	mov cl, 16d 					; set loop counter for 16 (length of bytesequence0)
+	rewrite: 						
+		mov al, byte ptr ds:[si] 	; load next byte from bytesequence0
+		mov byte ptr ds:[di], al 	; write it to next place in bytesequence1
+		inc si 						; si+=1
+		inc di 						; di+=1
+	loop rewrite
+
+
+	doxor: 						  	; iterate through bytesequence1, xor every byte with the folllowing byte, store result in bytesequence0
+	xor ax, ax  					; ax=0
+	xor cx, cx 						; cx=0
+	mov cl, 16d  					; set loop counter for 16
+	mov si, offset bytesequence1 	; set si for first byte of bytesequence1
+	mov di, offset bytesequence0 	; set di for first byte of bytesequence0
+	xorbytes:
+		mov al, byte ptr ds:[si] 	; read byte from bytesequence1
+		mov ah, byte ptr ds:[si+1] 	; read following byte
+		xor al, ah 					; xor al and ah, result is stored in al
+		mov byte ptr ds:[di], al 	; write al to next byte in bytesequence0
+		xor ax, ax 					; ax=0
+		inc si 						; si+=1
+		inc di  					; di+=1
+	loop xorbytes
+
+
+	nomod:
+	pop cx
+	pop ax
+ret
+
+makeallmoves: 						; return in dx position after all moves
+	push ax
+	push bx
+	push cx
+
+	mov ax, seg data
+	mov ds, ax
+	xor ax, ax
+
+ 	; in dx return position after all moves, by default - center (76)
+	xor dx, dx
+	mov dx, 76
+	; loop over 16 bytes of bytesequence0
+	mov cx, 16
+	mov bx, offset bytesequence0
+	makemoves:
+		mov al, byte ptr ds:[bx]
+		call makemovesbyte
+		inc bx
+	loop makemoves
+	pop cx
+	pop bx
+	pop ax
+ret
+
+makemovesbyte:						; input - byte in al, store in dx position after moves
+	
+	push bx
+	push cx
+	xor cx, cx
+	mov cl, 4 						; set loop counter to four iterations
+
+	makemove:
+		; make vertical moves
+		verticalmove:
+		mov bl, al
+		and bl, 00000010b
+		cmp bl, 00000010b 			; check second bit, if equals 1 make move down, else - make move up
+		JE makemovedown	
+
+		makemoveup:		 
+		call moveup 				; try to make move up, return target position in dx, don't modify chartable
+		JMP horizontalmove
+
+		makemovedown:				; same here, try to make move, return target position in dx
+		call movedown
+		JMP horizontalmove
+
+		horizontalmove:
+		mov bl, al
+		and bl, 00000001b 			; check first bit, if equals 1 make move right, else - make move left
+		cmp bl, 00000001b
+		JE makemoveright
+
+		makemoveleft:				; actual movement, try to make move to left(up/down), and if possible - move
+		call moveleft
+		JMP continue
+
+		makemoveright:				; actual movement, try to make move to right(up/down), and if possible - move
+		call moveright
+		JMP continue
+
+		continue:
+		push cx
+		mov cl, 2
+		shr al, cl
+		pop cx
+	loop makemove
+
+	pop cx
+	pop bx
+ret
+
+moveup:								; try to make move up, return potential position in dx, takes dx as argument
+
+	cmp dx, 16						; move one row up (dx-=17), if bishop is not in first row (dx>16 to make move)
+	JLE nomoveup					; if dx <=16 don't move
+	sub dx, 17						; if dx > 16 make move, return new position in dx
+
+	nomoveup:
+ret
+
+movedown:							; try to make move down, return potential position in dx, takes dx as argument
+
+	cmp dx, 136 					; move one row down (dx+=17), if bishop is not in last row (dx<136 to make move)
+	JGE nomovedown 					; if dx >=136 don't move
+	add dx, 17						; if dx < 136 make move, return new position in dx
+
+	nomovedown:
+ret
+
+moveleft: 							; try to make move left and modify chartable if any movement possible, input dx - current pos, output dx - pos after move
+	
+	push ax
+	push bx
+	push cx
+
+	mov ax, dx 						; make copy of dx in ax for checking dx % 17 
+	mov cl, 17						; set the divisor to 17
+	div cl
+	cmp ah, 0 						; al=dx/17, ah=dx%17
+	JE moveleftend 					; if dx%17!=0 we can move to the left (dx-=1), otherwise it means bishop is in 1st column and we can't make move to the left
+	dec dx
+
+	moveleftend:					; modify appropriate element in chartable
+	mov bx, dx 						
+	inc byte ptr ds:[chartable+bx]  ; chartable[dx]+=1
+
+	pop cx
+	pop bx
+	pop ax
+ret
+
+moveright:							; try to make move right and modify chartable if any movement possible, input dx - current pos, outpot dx - pos after move
+
+	push ax
+	push bx
+	push cx
+
+	mov ax, dx 						; make copy of dx in ax for checking dx % 17
+	mov cl, 17 						; set the divisor to 17
+	div cl
+	cmp ah, 16 						; check dx%17
+	JE moverightend 				; if dx%17!=16 we can move to the right (dx+=1), otherwise it means bishop is in last column and we can't make move to the right
+	inc dx 
+
+	moverightend: 					; modify appropriate element in chartable
+	mov bx, dx
+	inc byte ptr ds:[chartable+bx]	; chartable[dx]+=1
+
+	pop cx
+	pop bx
+	pop ax
+ret
+
+makefingerprint:					; for arg takes dx - last position of bishop, makes the fingerprint
+	push ax
+	push bx
+	push cx
+
+	mov ax, seg data
+	mov ds, ax
+	xor ax, ax
+
+	mov bx, offset chartable
+	mov di, 0
+	mov cx, 153
+	makefp:
+		cmp di, 76
+		JE changeS
+
+		cmp di, dx
+		JE changeE
+		JMP otherchars
+
+		changeS:
+		mov byte ptr ds:[bx+di], 'S'
+		JMP nextchar
+
+		changeE:
+		mov byte ptr ds:[bx+di], 'E'
+		JMP nextchar
+		
+		otherchars:
+		mov al, byte ptr ds:[bx+di]
+		cmp al, 0
+		JE space
+
+		cmp al, 14
+		JGE dash
+
+		call findrightchar
+		JMP nextchar
+		
+		dash:
+		mov byte ptr ds:[bx+di], '^'
+
+		space:
+		mov byte ptr ds:[bx+di], ' '
+		JMP nextchar
+
+		nextchar:
+		inc di
+	loop makefp
+
+
+	pop cx
+	pop bx
+	pop ax
+ret
+
+findrightchar:						; in al passed current number of visits, in bx passed offset of chartable, in di index of char
+	
+	char13:
+	cmp al, 13
+	JL char12
+	mov byte ptr ds:[bx+di], '/'
+	JMP endfind
+
+	char12:
+	cmp al, 12
+	JL char11
+	mov byte ptr ds:[bx+di], '#'
+	JMP endfind
+
+	char11:
+	cmp al, 11
+	JL char10
+	mov byte ptr ds:[bx+di], '&'
+	JMP endfind
+
+	char10:
+	cmp al, 10
+	JL char9
+	mov byte ptr ds:[bx+di], '%'
+	JMP endfind
+
+	char9:
+	cmp al, 9
+	JL char8
+	mov byte ptr ds:[bx+di], '@'
+	JMP endfind
+
+	char8:
+	cmp al, 8
+	JL char7
+	mov byte ptr ds:[bx+di], 'X'
+	JMP endfind
+
+	char7:
+	cmp al, 7
+	JL char6
+	mov byte ptr ds:[bx+di], 'O'
+	JMP endfind
+
+	char6:
+	cmp al, 6
+	JL char5
+	mov byte ptr ds:[bx+di], 'B'
+	JMP endfind
+
+	char5:
+	cmp al, 5
+	JL char4
+	mov byte ptr ds:[bx+di], '*'
+	JMP endfind
+
+	char4:
+	cmp al, 4
+	JL char3
+	mov byte ptr ds:[bx+di], '='
+	JMP endfind
+
+	char3:
+	cmp al, 3
+	JL char2
+	mov byte ptr ds:[bx+di], '+'
+	JMP endfind
+
+	char2:
+	cmp al, 2
+	JL char1
+	mov byte ptr ds:[bx+di], 'o'
+	JMP endfind
+
+	char1:
+	mov byte ptr ds:[bx+di], '.'
+	JMP endfind
+
+	
+	endfind:
+ret
+
+printfingerprint:					; print ascii art of fingerprint
+	push bx
+	push cx
+	push dx
+	push si
+	push di
+	
+	mov di, offset chartable		; load to di chartable offset
+		
+	mov dx, offset topframe			; print top frame part
+	call print
+	mov dx, offset newline
+	call print
+
+	mov cx, 9						; load to cx 9 (9 rows)
+	print_rows:
+		push cx						; push cx to stack, i will be using cx to print 17 chars in each row
+
+		mov dl, '|' 				; print element of frame
+		mov ah, 6 
+		int 21h 
+			
+		mov cx, 17					; load to cx 17 (for 17 columns/chars in row)
+		print_element:
+			mov dl, byte ptr ds:[di]
+			mov ah, 6 				; print char at di offset
+			int 21h
+				
+			inc di					; di+=1, for printing next char
+		loop print_element
+				
+		mov dl, '|' 				; print element of frame
+		mov ah, 6 
+		int 21h
+			
+		mov dx, offset newline 		; print new line
+		call print 
+
+		pop cx 						; pop cx for outer loop
+	loop print_rows
+		
+	mov dx, offset bottomframe													;na zakończenie jeszcze drukujemy dolną ramkę
+	call print
+
+	pop di	
+	pop si
+	pop dx
+	pop cx
+	pop bx
+ret
+
+
+
 
 
 ;###################################          Error handling          ################################### 
@@ -457,6 +840,8 @@ ret
 
 
 ;###################################          Print and exit          ################################### 
+
+
 print: 								;print variable from data segment
 	mov ax, seg data 				
     mov ds, ax 						; move data segment adress to ds 
